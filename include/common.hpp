ca9037c7cb90ea68cfae60fc36f485fd7fcbe373
chenyilyang <chenyilyang@hotmail.com>
2021-12-01 18:33:40 +0800

    FPS

--- include/common.hpp
+++ include/common.hpp
@@ -4,18 +4,51 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+glm::vec3 cameraPos(0.0f, 0.0f, 10.0f);
+glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+float cameraSpeed = 0.05f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float fps() {
+    float currFrame = glfwGetTime();
+    deltaTime = currFrame - lastFrame;
+    lastFrame = currFrame;
+    return 1.0f / deltaTime;
+}
 //windows size change callback
 void framebuffer_size_callback(GLFWwindow * window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 void common_process_input(GLFWwindow * window) {
+    cameraSpeed = 2.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        // cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp)); //same effect as next line
+        cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraUp, cameraFront));
     }
 }
 bool checkVertextShaderCompileResult(unsigned int vertexShader) {
